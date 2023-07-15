0e62a583d6d13e8987eb016f76542666ab5a6e65
Daniel Dos Santos Martins <danielsantosmartins.dev@gmail.com>
2023-07-15 16:47:25 -0300

    adicionando função de editar e salvar

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let listaDeItens = []
+let itemAEditar
 
 const form = document.getElementById("form-itens")
 const itensInput = document.querySelector("#receber-item")
@@ -54,6 +55,7 @@ function mostrarItem () {
                 <input type="text" class="is-size-5" value="${elemento.valor}"></input>
             </div>
             <div>
+                <button onclick = "salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button><i class="fa-regular is-clickable fa-pen-to-square editar"></i>    
                 <i class="fa-solid fa-trash is-clickable deletar"></i>
             </div>
             </li>`
@@ -70,4 +72,28 @@ function mostrarItem () {
         })
     })
 
+    const deletarObjetos = document.querySelectorAll(".deletar")
+
+    deletarObjetos.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+            listaDeItens.splice(valorDoElemento,1)
+            mostrarItem()
+        })
+    })
+
+    const editarItens = document.querySelectorAll(".editar")
+
+    editarItens.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            itemAEditar = evento.target.parentElement.parentElement.getAttribute('data-value')
+            mostrarItem()
+        })
+    })
+
+}
+
+function salvarEdicao() {
+    const itemEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text"]`)
+    console.log(itemEditado.value)
 }
