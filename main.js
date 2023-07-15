8072ff33124ff2d5edaf742c40f39afe3c19330b
Daniel Dos Santos Martins <danielsantosmartins.dev@gmail.com>
2023-07-15 16:09:48 -0300

    alterações adicionais de comodidade

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ let listaDeItens = []
 const form = document.getElementById("form-itens")
 const itensInput = document.querySelector("#receber-item")
 const ulItens = document.querySelector("#lista-de-itens")
+const ulItensComprados = document.querySelector("#itens-comprados")
 
 form.addEventListener("submit", function (evento) {
     evento.preventDefault()
     salvarItem()
     mostrarItem()
+    itensInput.focus()
 });
 
 function salvarItem() {
@@ -23,14 +25,28 @@ function salvarItem() {
         })
     }
         
-        console.log(listaDeItens)
+        itensInput.value = ''
     
 }
 
 function mostrarItem () {
     ulItens.innerHTML = ''
+    ulItensComprados.innerHTML = ''
 
     listaDeItens.forEach((elemento, index) => {
+        if(elemento.checar) {
+            ulItensComprados.innerHTML += `
+            <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+            <div>
+                <input type="checkbox" checked class="is-clickable" />  
+                <span class="itens-comprados is-size-5">${elemento.valor}</span>
+            </div>
+            <div>
+                <i class="fa-solid fa-trash is-clickable deletar"></i>
+            </div>
+            </li>
+            `
+        } else {
         ulItens.innerHTML += `
             <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
             <div>
@@ -41,6 +57,7 @@ function mostrarItem () {
                 <i class="fa-solid fa-trash is-clickable deletar"></i>
             </div>
             </li>`
+        }
     })
 
     const inputsCheck = document.querySelectorAll('input[type="checkbox"]')
@@ -49,7 +66,7 @@ function mostrarItem () {
         i.addEventListener('click', (evento) => {
             const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
             listaDeItens[valorDoElemento].checar = evento.target.checked
-            console.log(listaDeItens[valorDoElemento].checar)
+            mostrarItem()
         })
     })
 
